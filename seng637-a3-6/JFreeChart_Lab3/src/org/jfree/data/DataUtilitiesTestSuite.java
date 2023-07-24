fa90993f108e886af1c6b3628c01905ac29dbda2
mjfranci <m.francis.net@gmail.com>
2023-07-24 11:50:51 -0600

    Removed unused methods from RangeTestSuite, added 100% coverage of Constrain() method in RangeTestSuite, created DataUtilitiesTestSuite class with methods stubbed out

---
+++ seng637-a3-6/JFreeChart_Lab3/src/org/jfree/data/DataUtilitiesTestSuite.java
@@ -0,0 +1,64 @@
+package org.jfree.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DataUtilitiesTestSuite extends DataUtilities {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCalculateColumnTotalValues2DInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateColumnTotalValues2DIntIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateRowTotalValues2DInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalculateRowTotalValues2DIntIntArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNumberArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCreateNumberArray2D() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCumulativePercentages() {
+		fail("Not yet implemented");
+	}
+
+}
--- seng637-a3-6/JFreeChart_Lab3/src/org/jfree/data/RangeTestSuite.java
+++ seng637-a3-6/JFreeChart_Lab3/src/org/jfree/data/RangeTestSuite.java
@@ -27,118 +27,42 @@ public class RangeTestSuite {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public void testHashCode() {
-		int hashCode = exampleRange.hashCode();
-		assertEquals("Hash codes should be same", hashCode, exampleRange.hashCode());
-	}
+
 
 	@Test
-	public void testGetLowerBound() {
-		double lowerBound = exampleRange.getLowerBound();
-		assertEquals("LB should be same", lowerBound, exampleRange.getLowerBound(), 0.01d);
+	public void testConstrainNotContainingValue() {
+		assertEquals("Should return NaN", Double.NaN, exampleRange.constrain(Double.NaN), 0.01d);
 	}
 	
 	@Test
-	public void testGetUpperBound() {
-		double upperBound = exampleRange.getUpperBound();
-		assertEquals("UB should be same", upperBound, exampleRange.getUpperBound(), 0.01d);
+	public void testConstrainReturnUpperBound() {
+		assertEquals("Should return upper bound of 100", 100.0, exampleRange.constrain(101), 0.01d);
 	}
 	
 	@Test
-	public void testGetLength() {
-		double length = exampleRange.getLength();
-		assertEquals("length should be same", length, exampleRange.getLength(), 0.01d);
+	public void testConstrainReturnUpperBoundRobust() {
+		assertEquals("Should return upper bound of 100", 100.0, exampleRange.constrain(1000), 0.01d);
 	}
 	
 	@Test
-	public void testGetCentralValue() {
-		double cv = exampleRange.getCentralValue();
-		assertEquals("CV should be same", cv, exampleRange.getCentralValue(), 0.01d);
+	public void testConstrainReturnLowerBound() {
+		assertEquals("Should return lower bound of -100", -100.0, exampleRange.constrain(-101), 0.01d);
 	}
 	
 	@Test
-	public void testContains() {
-		double value = 0;
-		assertTrue("Should contain 0", exampleRange.contains(value));
+	public void testConstrainReturnLowerBoundRobust() {
+		assertEquals("Should return lower bound of -100", -100.0, exampleRange.constrain(-1000), 0.01d);
+	}
+	
+	@Test
+	public void testConstrainReturnInnerValue() {
+		assertEquals("Should return the input value of 0", 0.0, exampleRange.constrain(0), 0.01d);
+	}
+	
+	@Test
+	public void testConstrainReturnInnerValueRobust() {
+		assertEquals("Should return the input value of 50", 50.0, exampleRange.constrain(50), 0.01d);
 	}
 
-	@Test
-	public void testIntersectsDoubleDouble() {
-		assertTrue("Should intersect", exampleRange.intersects(-100, 100));
-	}
-
-	@Test
-	public void testIntersectsRange() {
-		Range newRange = new Range(-100,100);
-		assertTrue("should intersect", exampleRange.intersects(newRange));
-	}
-
-	@Test
-	public void testConstrain() {
-		assertEquals("Should constrain to value", 1.3, exampleRange.constrain(1.3), 0.01d);
-	}
-
-	@Test
-	public void testCombine() {
-		Range newRange = new Range(100,200);
-		Range finalRange = new Range(-100,200);
-		assertEquals("Ranges should be equal", finalRange, Range.combine(exampleRange, newRange));
-	}
-
-	@Test
-	public void testCombineIgnoringNaN() {
-		Range negRange = new Range(Double.NEGATIVE_INFINITY, 0);
-		Range posRange = new Range(0,Double.POSITIVE_INFINITY);
-		Range newRange = new Range(Double.NEGATIVE_INFINITY,Double.POSITIVE_INFINITY);
-		assertEquals("Ranges should be equal", newRange, Range.combineIgnoringNaN(posRange, negRange));
-	}
-
-	@Test
-	public void testExpandToInclude() {
-		Range newRange = new Range(-100,101);
-		assertEquals("Ranges should be equal", newRange, Range.expandToInclude(exampleRange, 101));
-	}
-
-	@Test
-	public void testExpand() {
-		Range newRange = new Range(-101,101);
-		assertEquals("Ranges should be equal", newRange, Range.expand(exampleRange, -101, 101));
-	}
-
-	@Test
-	public void testShiftRangeDouble() {
-		Range newRange = new Range(-95,105);
-		assertEquals("Ranges should be equal", newRange, Range.shift(exampleRange, 5));
-	}
-
-	@Test
-	public void testShiftRangeDoubleBoolean() {
-		Range newRange = new Range(-95,105);
-		assertEquals("Ranges should be equal", newRange, Range.shift(exampleRange, 5, true));
-	}
-
-	@Test
-	public void testScale() {
-		Range newRange = new Range(-200,200);
-		assertEquals("Ranges should be equal", newRange, Range.scale(exampleRange, 2));
-	}
-
-	@Test
-	public void testEqualsObject() {
-		assertTrue("Ranges should be equal", exampleRange.equals(exampleRange));
-	}
-
-	@Test
-	public void testIsNaNRange() {
-		Range newRange = new Range(Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
-		assertTrue("Bounds should not be a number", newRange.isNaNRange());
-	}
-
-	@Test
-	public void testToString() {
-		String rangeString = exampleRange.toString();
-		assertSame("Strings should be same", rangeString, exampleRange.toString());
-	}
 
 }
