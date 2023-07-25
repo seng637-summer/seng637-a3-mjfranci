8db0d53816695901bd053d96af374f087439679b
jcharlpretorius <pretorius.jeancharl@gmail.com>
2023-07-24 22:13:51 -0600

    add imports

--- seng637-a3-6/JFreeChart_Lab3/src/org/jfree/data/DataUtilitiesTestSuite.java
+++ seng637-a3-6/JFreeChart_Lab3/src/org/jfree/data/DataUtilitiesTestSuite.java
@@ -1,6 +1,15 @@
 package org.jfree.data;
 
 import static org.junit.Assert.*;
+import java.security.InvalidParameterException;
+
+import org.jfree.data.DataUtilities;
+import org.jfree.data.DefaultKeyedValues;
+import org.jfree.data.KeyedValues;
+import org.jfree.data.Values2D;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.junit.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -9,7 +18,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class DataUtilitiesTestSuite extends DataUtilities {
-	
+
 	private Mockery mockingContext;
 	private KeyedValues keyedValues;
 	private Values2D values;
